4c2eed4a86a1e12c7f8ebc222643b2ea468d1b00
star-roy <dibyaranjannayak04@gmail.com>
2025-08-25 15:57:47 +0530

    fix: Update API proxy path to include full URL for backend

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': {
+      'https://zephyra-backend-1.onrender.com/api': {
         target: 'https://zephyra-backend-1.onrender.com',
         changeOrigin: true,
       }
